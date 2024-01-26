c5f1cc4767cdc67112f9bc6e11d55bc58c0b19fc
delcroip <patrick.delcroix@swisstph.ch>
2024-01-26 11:06:03 +0100

    fixes

--- deploy_coremis.sh
+++ deploy_coremis.sh
@@ -6,10 +6,12 @@ cp .env.openSearch.example .env.openSearch
 
 docker compose -f docker-compose.coremis.yml up -d db
 set -a # automatically export all variables
+source .env
 source .env.lightning
 set +a
-docker compose -f docker-compose.coremis.yml run -e  PGPASSWORD=${POSTGRES_PASSWORD} --rm db createdb -h db -U ${POSTGRES_USER}  createdb ${POSTGRES_NAME}
-docker compose -f docker-compose.coremis.yml run web mix run imisSetupScripts/imisSetup.exs
-docker compose -f docker-compose.coremis.yml
+docker compose -f docker-compose.coremis.yml run -e  PGPASSWORD=${POSTGRES_PASSWORD} --rm db createdb -h db -U ${POSTGRES_USER}  ${POSTGRES_DB}
+docker compose -f docker-compose.coremis.yml run --rm  web mix ecto.migrate
+docker compose -f docker-compose.coremis.yml run --rm web mix run imisSetupScripts/imisSetup.exs
+docker compose -f docker-compose.coremis.yml up -d
 
 
