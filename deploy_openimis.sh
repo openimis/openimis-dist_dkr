29b6f21336075c286ae20a64eafae7ec62e9c867
Patrick Delcroix <patrick.delcroix@swisstph.ch>
2024-04-17 17:06:34 +0200

    Feature/modular docker compose (#57)
    * modular config
    
    * modular compose

---
+++ deploy_openimis.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#rename .env
+if [[ -f '.env' ]]
+then
+echo "Using existing env files"
+else
+echo "creating env files from example"
+cp .env.example .env
+cp .env.lightning.example .env.lightning
+cp .env.openSearch.example .env.openSearch
+fi
+
+
+if [[ -f '.init.lock' ]]
+then
+echo "initialisation already done"
+else
+echo "initialisation"
+
+docker compose  up -d db
+#set -a # automatically export all variables
+source .env
+source .env.lightning
+#set +a
+docker compose  run -e  PGPASSWORD=${POSTGRES_PASSWORD} --rm db createdb -h db -U ${POSTGRES_USER}  ${POSTGRES_DB}
+set -e
+docker compose  run --rm  web mix ecto.migrate
+docker compose  run --rm web mix run imisSetupScripts/imisSetup.exs
+#TODO init opensearch dashboard with API/ manage command
+echo "connect to https://{DOMAIN}"
+echo "then go to https://{DOMAIN}/opensearch"
+echo "then go in manage / saved object / import to import the openSearch dashboard"
+touch '.init.lock' 
+fi
+docker compose up -d
+
+
