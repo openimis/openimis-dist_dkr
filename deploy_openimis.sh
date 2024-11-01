7291096ef3168480f570e396f23e39548c51db79
Patrick Delcroix <delcroip@gmail.com>
2024-11-01 16:03:11 +0100

    updating ENV var

--- deploy_openimis.sh
+++ deploy_openimis.sh
@@ -6,8 +6,9 @@ echo "Using existing env files"
 else
 echo "creating env files from example"
 cp .env.example .env
-cp .env.lightning.example .env.lightning
+cp .env.redis.example .env.redis
 cp .env.openSearch.example .env.openSearch
+cp .env.database.example .env.database
 fi
 
 
@@ -18,15 +19,15 @@ else
 echo "initialisation"
 
 docker compose  up -d db
-#set -a # automatically export all variables
-source .env
-source .env.lightning
-#set +a
-docker compose  run -e  PGPASSWORD=${POSTGRES_PASSWORD} --rm db createdb -h db -U ${POSTGRES_USER}  ${POSTGRES_DB}
-set -e
-docker compose  run --rm  web mix ecto.migrate
-docker compose  run --rm web mix run imisSetupScripts/imisSetup.exs
-#TODO init OpenSearch dashboard with API/ manage command
+# #set -a # automatically export all variables
+# source .env
+# source .env.lightning
+# #set +a
+# docker compose  run -e  PGPASSWORD=${POSTGRES_PASSWORD} --rm db createdb -h db -U ${POSTGRES_USER}  ${POSTGRES_DB}
+# set -e
+# docker compose  run --rm  web mix ecto.migrate
+# docker compose  run --rm web mix run imisSetupScripts/imisSetup.exs
+# #TODO init OpenSearch dashboard with API/ manage command
 echo "connect to https://{DOMAIN}"
 echo "then go to https://{DOMAIN}/opensearch"
 echo "then go in manage / saved object / import to import the OpenSearch dashboard"
