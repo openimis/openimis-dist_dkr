9171f529c62376a3ca7d8812fb3cb10b97f0b4f7
vaneck237 <vaneckbilongo@gmail.com>
2026-06-19 13:05:54 +0100

    remove commentary

--- cypress/e2e/claim.cy.js
+++ cypress/e2e/claim.cy.js
@@ -12,26 +12,26 @@ describe('Test for claim restoration', () => {
         cy.login();
     });
 
-    // it('Claim restoration workflow', () => {
-    //     cy.openFirstRejectedClaim();
-    //     cy.restoreClaim(data.restore.code);
-    //     cy.verifyClaim(data.restore.code);
-    // });
+    it('Claim restoration workflow', () => {
+        cy.openFirstRejectedClaim();
+        cy.restoreClaim(data.restore.code);
+        cy.verifyClaim(data.restore.code);
+    });
 
-    // it('Create claim with complex product workflow', () => {
-    //     cy.goToList('Claims', 'Health Facility Claims');
-    //     cy.createClaim(data.claim, 'complex');
-    //     cy.verifyClaim(data.claim.code);
-    //     cy.openRow('Claim No.', data.claim.code);
-    //     cy.verifyContent(data.claim);
-    // });
+    it('Create claim with complex product workflow', () => {
+        cy.goToList('Claims', 'Health Facility Claims');
+        cy.createClaim(data.claim, 'complex');
+        cy.verifyClaim(data.claim.code);
+        cy.openRow('Claim No.', data.claim.code);
+        cy.verifyContent(data.claim);
+    });
 
-    // it('Claim duplication workflow', () => {
-    //     cy.searchClaim(data.claim);
-    //     cy.openRow('Claim No.', data.claim.code);
-    //     cy.duplicateClaim(data.duplicate);
-    //     cy.verifyClaim(data.duplicate.code);
-    // });
+    it('Claim duplication workflow', () => {
+        cy.searchClaim(data.claim);
+        cy.openRow('Claim No.', data.claim.code);
+        cy.duplicateClaim(data.duplicate);
+        cy.verifyClaim(data.duplicate.code);
+    });
 
     it('Claim review workflow', () => {
         cy.goToList('Claims', 'Health Facility Claims');
